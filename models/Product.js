73e3904a2507b733f177d4ab6d07643388b9ef01
Jalen Moore <100977121+moorejalen2800@users.noreply.github.com>
2022-05-26 00:39:49 -0400

    Update Product.js
    defined column and finished off code

--- models/Product.js
+++ models/Product.js
@@ -9,20 +9,53 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-     // define columns
+    // define columns
+
     id: {
+
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
       autoIncrement: true,
+
     },
-    category_name: {
+    product_name: {
       type: DataTypes.STRING,
       allowNull: false,
+
+
+  },
+  price: {
+    type: DataTypes.DECIMAL,
+    allowNull: false,
+    validate: {
+      isDecimal: true,
+    },
+    },
+
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+
+      },
+    },
+
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id',
+
+      },
+
     }
 
-   
   },
+
+
   {
     sequelize,
     timestamps: false,
@@ -33,3 +66,4 @@ Product.init(
 );
 
 module.exports = Product;
+
